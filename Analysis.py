24ddcdf96b17f3fdcd23e1d32662e65c51c54f1c
brianlam38 <brian.lam38@gmail.com>
2017-08-22 10:48:38 +1000

    adding bitcoin pricing data code, Jupyter Notebook file

---
+++ Analysis.py
@@ -0,0 +1,34 @@
+import os
+import numpy as np
+import pandas as pd
+import pickle
+import quandl
+from datetime import datetime
+
+import plotly.offline as py
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+py.init_notebook_mode(connected=True)
+
+def get_quandl_data(quandl_id):
+    '''Download and cache Quandl dataseries'''
+    cache_path = '{}.pkl'.format(quandl_id).replace('/','-')
+    try:
+        f = open(cache_path, 'rb')
+        df = pickle.load(f)   
+        print('Loaded {} from cache'.format(quandl_id))
+    except (OSError, IOError) as e:
+        print('Downloading {} from Quandl'.format(quandl_id))
+        df = quandl.get(quandl_id, returns="pandas")
+        df.to_pickle(cache_path)
+        print('Cached {} at {}'.format(quandl_id, cache_path))
+    return df
+
+# Pull Kraken BTC price exchange data
+btc_usd_price_kraken = get_quandl_data('BCHARTS/KRAKENUSD')
+
+btc_usd_price_kraken.head()
+
+# Chart the BTC pricing data
+btc_trace = go.Scatter(x=btc_usd_price_kraken.index, y=btc_usd_price_kraken['Weighted Price'])
+py.iplot([btc_trace])
